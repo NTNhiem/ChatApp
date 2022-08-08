d5c029b16f2c9662e1a83acf4d47ebd5e2bea4cd
NguyenHuuTien2000 <tien.nguyenhuu.set18@eiu.edu.vn>
2022-08-08 20:02:27 +0700

    Minor fixes

--- src/Services/UserService.java
+++ src/Services/UserService.java
@@ -46,7 +46,7 @@ public class UserService {
         User user = new User(id, gender,dob, firstName, lastName, username, password, salt);
         User tempUser = this.findUserByID(user.getId());
         List<User> foundUsers = findUserByUsername(username);
-        if (tempUser == null && userList.isEmpty()) {
+        if (tempUser == null && foundUsers.isEmpty()) {
             this.userList.add(user);
             this.dataStorage.saveUserList(this.userList);
             return true;
